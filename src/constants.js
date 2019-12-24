3667d4f858523a7b9c4479543d76b2922c822096
Sean Rice <seanrice2015@gmail.com>
2019-12-24 10:28:40 -0800

    Updated header text

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 export const HEADER_GREET = 
 (
-  <span>Hi I'm Sean <span role="img" aria-label="Wave Hand">👋</span><br></br> 
-    I'm a senior at University of Wisconsin, and I have a passion for <span className="has-text-weight-semibold">full-stack development</span>.
+  <span>Hi I'm Sean <span role="img" aria-label="Wave Hand">👋</span><br /> 
+    I'm a Software Engineer based in San Francisco.<br />
+    I <span role="img" aria-label="Heart">❤️</span><span className="has-text-weight-semibold"> security and full-stack development</span>.
   </span>
 )
