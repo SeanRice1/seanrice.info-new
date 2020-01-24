d9c9c90e79635886b737e1b6a19a7897acecf907
Sean Rice <seanrice2015@gmail.com>
2020-01-24 13:08:05 -0800

    added basic google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 
 import Intro from './components/header/header';
 import Content from './components/content';
 import Footer from './components/footer/footer';
+
+ReactGA.initialize('UA-156886618-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => (
   <div>
